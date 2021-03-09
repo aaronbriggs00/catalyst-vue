63c9bdcfd96e7dbe4911ff97ccdc3892fcdd5223
AaronBriggs <aaron00.briggs@gmail.com>
2021-03-09 15:23:31 -0600

    initial commit

---
+++ src/views/PostsShow.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="posts-show">
+    <h1>{{ post }}</h1>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      message: "Welcome to a post",
+      post: [],
+    };
+  },
+  created: function() {
+    this.apiCall();
+  },
+  methods: {
+    apiCall: function() {
+      axios.get(`/api/posts/${this.$route.params.id}`).then((response) => {
+        console.log(response.data);
+        this.post = response.data;
+      });
+    },
+  },
+};
+</script>
